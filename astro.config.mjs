906dbc5cc9c071436cad519dcf4e442c9f0db077
JiaXinTang <2174064279@qq.com>
2026-03-05 18:38:39 +0800

    update

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-// astro.config.mjs
-
-REPO_BASE = '/JIABlog/';
-export default defineConfig({
-  site: 'JiaXinTang-xiang.github.io',
-  base: REPO_BASE,
-  trailingSlash: 'always',
-  output: 'static',
-})
